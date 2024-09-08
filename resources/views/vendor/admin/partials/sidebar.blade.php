10b12de52e5a48ba96cacf10132340f1938bf546
zhangwuphp <zhang.wu@indzz.cn>
2024-09-09 00:10:34 +0800

    main:点单页面添加

--- resources/views/vendor/admin/partials/sidebar.blade.php
+++ resources/views/vendor/admin/partials/sidebar.blade.php
@@ -7,7 +7,7 @@
                 <ul class="nav navbar-nav flex-row">
                     <li class="nav-item mr-auto">
                         <a href="{{ admin_url('/') }}" class="navbar-brand waves-effect waves-light">
-                            <span class="logo-lg">库存管理系统</span>
+                            <span class="logo-lg">ERP</span>
                                 {{--<span class="logo-mini">{!! config('admin.logo-mini') !!}</span>
                                 <span class="logo-lg">{!! config('admin.logo') !!}</span>--}}
                         </a>
