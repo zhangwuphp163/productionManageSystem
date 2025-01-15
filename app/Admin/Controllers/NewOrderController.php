f201e721fe856841002f74a19dcf3f1e4412fdcd
KGHGROUP\TZhangW <tom.w.zhang@kerrylogistics.com>
2025-01-15 11:29:07 +0800

    main:fixed bug new - order list

--- app/Admin/Controllers/NewOrderController.php
+++ app/Admin/Controllers/NewOrderController.php
@@ -68,7 +68,7 @@ class NewOrderController extends AdminController
             //$grid->model()->resetOrderBy();
             //$grid->column('id')->sortable();
             $grid->column('platform_number','平台/系统/运单')->display(function(){
-                return $this->platform_number.".'<br/>'.$this->system_number."<br/".<a href='https://t.17track.net/zh-cn#nums={$this->tracking_number}' target='_blank'>".$this->tracking_number."</a>";
+                return $this->platform_number."<br/>".$this->system_number."<br/><a href='https://t.17track.net/zh-cn#nums={$this->tracking_number}' target='_blank'>".$this->tracking_number."</a>";
             })->filter();//->sortable();
             $grid->column('images',"订单图片")->display(function ($pictures){
                 return $pictures?\GuzzleHttp\json_decode($pictures, true):[];
